0d9327ba049ad6c9fb6d0970aee446471e24239e
Rian <godamri@gmail.com>
2023-02-02 08:09:05 +0000

    catch exceptions

--- install_script.sh
+++ install_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#git clone https://github.com/godamri/connect.git
-#cd ./connect
-#rm -rf .git
-#composer install
+git clone https://github.com/godamri/connect.git
+cd ./connect
+rm -rf .git
+composer install
 echo '1.0.0' | php application app:build connect
 sudo mkdir -p /opt/connect
 sudo cp $(pwd)/builds/connect /opt/connect
