0e2bf7d0169cc67c74df31b713dbe361d6509e6c
Rian <godamri@gmail.com>
2023-02-02 08:26:50 +0000

    add clone connection menu

--- app/Commands/SshCommand.php
+++ app/Commands/SshCommand.php
@@ -39,7 +39,7 @@ class SshCommand extends Command
             'Register New Connection',
             'Delete Connection',
         ])->open();
-        if( !is_int($command) ) {
+        if (!is_int($command)) {
             return;
         }
 
@@ -90,13 +90,13 @@ class SshCommand extends Command
         $names = [];
         foreach ($this->connections as $k => $connection) {
             $name = $connection['name'];
-            if( $connection['port_forwarding'] === 'yes'){
+            if ($connection['port_forwarding'] === 'yes') {
                 $name .= ' [Port Forwarding]';
             }
             $names[$k] = $name;
         }
         $connect = $this->menu('All Connections', $names)->open();
-        if( !is_int($connect) ) {
+        if (!is_int($connect)) {
             return;
         }
         $connect = $this->connections[$connect];
@@ -108,37 +108,41 @@ class SshCommand extends Command
         $names = [];
         foreach ($this->connections as $k => $connection) {
             $name = $connection['name'];
-            if( $connection['port_forwarding'] === 'yes'){
+            if ($connection['port_forwarding'] === 'yes') {
                 $name .= ' [Port Forwarding]';
             }
             $names[$k] = $name;
         }
         $connect = $this->menu('All Connections', $names)->open();
-        if( !is_int($connect) ) {
+        if (!is_int($connect)) {
             return;
         }
         $connect = $this->connections[$connect];
-        
+
         $action = $this->menu('Action', [
             'Info',
-            'Connect'
+            'Connect',
+            'Clone'
         ])->open();
 
-        if($action === 0) {
+        if ($action === 0) {
             $this->info('Connection Info:');
             $this->line('Name: ' . $connect['name']);
             $this->line('Host: ' . $connect['host']);
             $this->line('Port: ' . $connect['port']);
-            if( $connect['port_forwarding'] === 'yes'){
+            if ($connect['port_forwarding'] === 'yes') {
                 $this->line('Port Forwarding');
-                $this->line('Host: '. $connect['host']);
-                $this->line('Method: '. $connect['pf_method']);
-                $this->line('Local: '. $connect['local_port']);
-                $this->line('Remote: '. $connect['remote_port']);
+                $this->line('Host: ' . $connect['host']);
+                $this->line('Method: ' . $connect['pf_method']);
+                $this->line('Local: ' . $connect['local_port']);
+                $this->line('Remote: ' . $connect['remote_port']);
             }
         }
-        if($action === 1) {
+        if ($action === 1) {
             $this->connect($connect);
+        }
+        if ($action === 2) {
+            $this->cloneConnection($connect);
         }
 
         return;
@@ -150,10 +154,13 @@ class SshCommand extends Command
             $names[$k] = $connection['name'];
         }
         $connect = $this->menu('All Connections', $names)->open();
-        if(is_int($connect)) {
+        if (is_int($connect)) {
             $selected = $this->connections[$connect];
-            if( $selected['type'] === 'public_key') {
-                try{unlink($selected['private_key']);} catch(\Exception $e){}
+            if ($selected['type'] === 'public_key') {
+                try {
+                    unlink($selected['private_key']);
+                } catch (\Exception $e) {
+                }
             }
             unset($this->connections[$connect]);
             file_put_contents($this->config, json_encode($this->connections));
@@ -161,15 +168,18 @@ class SshCommand extends Command
         $this->handle();
         return;
     }
-    private function registerConnection()
+    private function registerConnection($message = null)
     {
+        if ($message) {
+            $this->line($message);
+        }
         $this->info('Register New Connection');
         $conn = [];
         $conn['name'] = $this->ask('name');
         $conn['slug'] = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $conn['name'])));
         foreach ($this->connections as $cn) {
             if ($cn['slug'] === $conn['slug']) {
-                $this->error('Connection with same name has been found');
+                $this->registerConnection('Connection with same name has been found');
                 return;
             }
         }
@@ -202,7 +212,7 @@ class SshCommand extends Command
         $this->info('Password: user password / key phasprase');
         $conn['password'] = $this->ask('password', null);
         $conn['port_forwarding'] = strtolower($this->ask('port_forwarding', 'no'));
-        if( $conn['port_forwarding'] === 'yes' ) {
+        if ($conn['port_forwarding'] === 'yes') {
             $this->info('Port Forwarding Method: local / remote');
             $conn['pf_method'] = strtolower($this->ask('pf_method', 'local'));
             $conn['local_port'] = $this->ask('local_port', 8088);
@@ -214,13 +224,72 @@ class SshCommand extends Command
         $this->info(sprintf('Connection %s saved', $conn['name']));
         return;
     }
+    private function cloneConnection($conn, $message = null)
+    {
+        if ($message) {
+            $this->line($message);
+        }
+        $this->info('Register New Connection');
+        $conn['name'] = $this->ask('name', $conn['name']);
+        $conn['slug'] = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $conn['name'])));
+        foreach ($this->connections as $cn) {
+            if ($cn['slug'] === $conn['slug']) {
+                $this->cloneConnection($conn, 'Connection with same name has been found');
+                return;
+            }
+        }
+        $conn['host'] = $this->ask('host', $conn['host']);
+        $conn['port'] = $this->ask('port', $conn['port']);
+        $conn['user'] = $this->ask('user', $conn['user']);
+        $this->info('Authentication Type: public_key / password');
+        $conn['type'] = $this->ask('type', $conn['type']);
+        if (strtolower($conn['type']) === 'public_key') {
+            $keys = scandir(getenv('HOME') . '/.ssh/');
+            $matchedKeys = [];
+            foreach ($keys as $key) {
+                if ($key !== '.' && $key !== '..' && substr($key, -4) === '.pub') {
+                    $matchedKeys[] = $key;
+                }
+            }
+            $selectedKey = count($matchedKeys) > 0 ? (getenv('HOME') . '/.ssh/' . substr($matchedKeys[0], 0, -4)) : '';
+            $conn['private_key'] = $this->ask('private_key', $selectedKey);
+            if (!preg_match('/^\S.*\s.*\S$/', $conn['private_key'])) {
+                $conn['private_key'] = file_get_contents($selectedKey);
+            }
+            $privKey = $conn['slug'] . '.key';
+            if (!is_file($this->home . '/keys/' . $privKey)) {
+                fopen($this->home . '/keys/' . $privKey, 'w');
+            }
+            file_put_contents($this->home . '/keys/' . $privKey, $conn['private_key']);
+            chmod($this->home . '/keys/' . $privKey, 0600);
+            $conn['private_key'] = $this->home . '/keys/' . $privKey;
+        }
+        $this->info('Password: user password / key phasprase');
+        $pass = $this->ask('password', null);
+        if (is_null($pass) && !is_null($conn['password'])) {
+            $conn['password'] = $pass;
+        }
+        $conn['port_forwarding'] = strtolower($this->ask('port_forwarding', $conn['port_forwarding']));
+        if ($conn['port_forwarding'] === 'yes') {
+            $this->info('Port Forwarding Method: local / remote');
+            $conn['pf_method'] = strtolower($this->ask('pf_method', $conn['pf_method'] ?? 'local'));
+            $conn['local_port'] = $this->ask('local_port', $conn['local_port'] ?? 8088);
+            $conn['remote_port'] = $this->ask('remote_port', $conn['remote_port'] ?? 80);
+        }
+        $this->connections[] = $conn;
+        file_put_contents($this->config, json_encode($this->connections));
 
-    private function connect($config) {
+        $this->info(sprintf('Connection %s saved', $conn['name']));
+        return;
+    }
+
+    private function connect($config)
+    {
         $forwarding = '';
-        if($config['port_forwarding']==='yes') {
-            $pfMethod = ($config['pf_method']==='local')?'L':'R';
-            $pfHost = ($config['pf_method']==='local')?'127.0.0.1':$config['host'];
-            $forwarding = sprintf('-%s %s:%s:%s',$pfMethod, $config['local_port'], $pfHost, $config['remote_port']);
+        if ($config['port_forwarding'] === 'yes') {
+            $pfMethod = ($config['pf_method'] === 'local') ? 'L' : 'R';
+            $pfHost = ($config['pf_method'] === 'local') ? '127.0.0.1' : $config['host'];
+            $forwarding = sprintf('-%s %s:%s:%s', $pfMethod, $config['local_port'], $pfHost, $config['remote_port']);
         }
         if ($config['type'] === 'public_key') {
             passthru(sprintf(
